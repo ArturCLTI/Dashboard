b18c6a51d84abe54fa804c6528b13147e99bf208
Artur da Costa <arturclti@outlook.com>
2023-03-13 15:45:43 -0300

    Dashboard

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 //document.getElementById("personagens").innerHTML = 22;
-const personagenscontador = document.querySelector("#personagens");
-const luacontador = document.querySelector("#luas");
-const planetascontador = document.querySelector("#planetas");
-const navescontador = document.querySelector("#naves");
+const personagensContador = document.querySelector("#personagens");
+const luasContador = document.querySelector("#luas");
+const planetasContador = document.querySelector("#planetas");
+const navesContador = document.querySelector("#naves");
+
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawChart);
+
+async function drawChart() {
+  const response = await swapiGet("vehicles/");
+  const vehiclesArray = response.results;
+  console.log(vehiclesArray);
+
+  const dataArray = [];
+  dataArray.push(["Veículos", "Passageiros"]);
+  vehiclesArray.forEach((vehicle) => {
+    dataArray.push([vehicle.name, Number(vehicle.passengers)]);
+  });
+
+  var data = google.visualization.arrayToDataTable(dataArray);
+
+  var options = {
+    title: "Passageiros",
+  };
+
+  var chart = new google.visualization.PieChart(
+    document.getElementById("piechart")
+  );
+
+  chart.draw(data, options);
+}
 
 /* async function swapiGet(params) {
   const response = await fetch(`https://swapi.dev/api/${params}`);
@@ -36,8 +63,6 @@ repassandoValores(); */
 async function swapiGet(params) {
   const response = await fetch(`https://swapi.dev/api/${params}`);
   const data = await response.json();
-  console.log(response);
-
   return data;
 }
 
@@ -48,11 +73,29 @@ function repassandoValores() {
     swapiGet("planets"),
     swapiGet("starships"),
   ]).then((response) => {
-    personagenscontador.innerHTML = `${response[0].count}`;
-    luacontador.innerHTML = `${response[1].count}`;
-    planetascontador.innerHTML = `${response[2].count}`;
-    navescontador.innerHTML = `${response[3].count}`;
+    personagensContador.innerHTML = `${response[0].count}`;
+    luasContador.innerHTML = `${response[1].count}`;
+    planetasContador.innerHTML = `${response[2].count}`;
+    navesContador.innerHTML = `${response[3].count}`;
+  });
+}
+repassandoValores();
+
+async function preenchercatalago() {
+  const response = await swapiGet("films");
+  const tableData = response.results;
+  console.log(tableData);
+  console.log(response);
+  tableData.forEach((film) => {
+    document.querySelector("#preencherTabela").innerHTML += `
+      <tr>
+        <td>${film.title}</td>
+        <td>${film.release_date}</td>
+        <td>${film.director}</td>
+        <td>${film.episode_id}</td>
+      </tr>
+    `;
   });
 }
 
-repassandoValores();
+preenchercatalago();
